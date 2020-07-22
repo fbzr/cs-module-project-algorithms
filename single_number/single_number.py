793adcad09d2a9c96db6543d4afbb5293b8189aa
fbzr <fabricio.spb@gmail.com>
2020-07-22 10:07:48 -0700

    single number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -1,11 +1,25 @@
-'''
-Input: a List of integers where every int except one shows up twice
-Returns: an integer
-'''
 def single_number(arr):
-    # Your code here
+    '''
+    Input: a List of integers where every int except one shows up twice
+    Returns: an integer
 
-    pass
+    >>> print(single_number([1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]))
+    >>> 9
+    '''
+    # get first value in array
+    current = arr[0]
+
+    # loop through the rest of the array until find the same value
+    for index, number in enumerate(arr[1:], 1):
+        # if second value is found, swap it with the value at current index + 1 (1)
+        if number == current:
+            arr[1], arr[index] = arr[index], arr[1]
+            # if found, repeat same process from current index + 2 after swap
+            return single_number(arr[2:])
+    
+    # if the same value is not found return value
+    return current    
+    
 
 
 if __name__ == '__main__':
