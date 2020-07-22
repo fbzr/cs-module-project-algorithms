385c40ee14c9243fcf2b710326504c8cb5e67abc
fbzr <fabricio.spb@gmail.com>
2020-07-22 11:22:13 -0700

    moving zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -1,11 +1,29 @@
 '''
 Input: a List of integers
 Returns: a List of integers
+
+Sample input: [3, 0, 1, 0, -2]
+Expected output: 3
+Expected output array state: [3, 1, -2, 0, 0]
+
+Sample input: [4, 2, 1, 5]
+Expected output: 4
+Expected output array state: [4, 2, 1, 5]
 '''
 def moving_zeroes(arr):
-    # Your code here
+    # Write a function that takes an array of integers and moves each non-zero integer to the left side of the array, then returns the altered array. The order of the non-zero integers does not matter in the mutated array.
 
-    pass
+    # loop through the list until find a non zero value
+    # move that value to position 0
+
+    # repeat the process till the end of list
+
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            value = arr.pop(i)
+            arr.insert(0, value)
+    
+    return arr
 
 
 if __name__ == '__main__':
