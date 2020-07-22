62dbc2e2e181b367a692039c87c1acced6b32808
fbzr <fabricio.spb@gmail.com>
2020-07-22 14:25:53 -0700

    eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,11 +1,33 @@
 '''
 Input: an integer
 Returns: an integer
+
+# if n == 10: 274 possible ways
+# if n == 9: 149 possible ways
+# if n == 8: 81 possible ways
+# if n == 7: 44 possible ways
+# if n == 6: 24 possible ways
+# if n == 5: 13 possible ways
+# if n == 4: 7 possible ways
+# if n == 3: 4 possible ways
+# if n == 2: 2 possible ways
+# if n == 1: 1 possible way
+
 '''
 def eating_cookies(n):
     # Your code here
+    # receives number of cookies (n)
+    # can eat up to 3 cookies at once
+    if n < 0:
+        return 0
 
-    pass
+    if n == 1 or n == 0:
+        return 1
+
+    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+    
+    
+    # return number of possible ways to eat all the cookies
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
