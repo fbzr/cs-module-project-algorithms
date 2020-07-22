83ca201b74d044c6620c77ef5dc428f2ed29cc7e
fbzr <fabricio.spb@gmail.com>
2020-07-22 11:50:34 -0700

    product of all other numbers

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -1,16 +1,32 @@
 '''
 Input: a List of integers
 Returns: a List of integers
+
+Write a function that receives an array of integers and returns an array consisting of the product of all numbers in the array _except_ the number at that index. 
+
+For example, given 
+[1, 7, 3, 4]
+your function should return 
+[84, 12, 28, 21]
 '''
 def product_of_all_other_numbers(arr):
-    # Your code here
+    
+    # for each number you need to return multiplication of all numbers in list but that number
+    
+    # loop through list and multiply all numbers
+    total = 1
+    for x in arr:
+        total *= x
+    # create list new list where: each item is the total divided by its value
+    result = [total // value for value in arr]
 
-    pass
+    # return new list
+    return result
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    # arr = [1, 2, 3, 4, 5]
-    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    arr = [1, 2, 3, 4, 5]
+    # arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
     print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
